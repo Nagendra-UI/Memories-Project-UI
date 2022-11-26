bafe767e6a54fd0ef4657ee434af1aaaee4b3a7b
Nagendra-UI <nagendra.bmail@gmail.com>
2022-11-26 10:45:13 +0530

    setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import { Container, AppBar, Typography, Grow, Grid } from '@mui/material'
+
+function App() {
+  return (
+    <Container maxWidth="lg">
+      <h1>fdfsfsg</h1>
+
+    </Container>
+  );
+}
+
+export default App;
